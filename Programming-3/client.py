0d9e609e8a3e836e4dd75dbac29b12c47b92b1fe
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-04-02 19:34:59 -0400

    Added Code
    Added Code

--- Programming-3/client.py
+++ Programming-3/client.py
@@ -3,6 +3,7 @@ import os
 import csv
 import argparse
 import ipaddress
+import select
 from util import TestErrorCode
 #TODO import necessary library
 
@@ -41,7 +42,7 @@ def main():
         clientSock.connect((args.server_ip, HTTP_PORT))
     except socket.error as e:
         return TestErrorCode.TEST_ERROR_HTTP_CONNECT_FAILED
-    clientSock.setblocking(0)
+    #clientSock.setblocking(0)
 
     uri = "/test_dependency"
     dependencyPath = os.getcwd() + "/www" + uri
@@ -56,42 +57,40 @@ def main():
     clientSock.close()
 
 
-def sendReq(request,clientSock,dep):
+import select  # Import the select module
+
+def sendReq(request, clientSock, dep):
     clientSock.send(request.encode())
     recvData = b''  # Initialize empty bytes object to store received data
     total_received = 0
     delimiter_found = False
-    while True:
-        chunk = clientSock.recv(BUF_SIZE)
-        if not chunk:
-            break
-        recvData += chunk
-        if not delimiter_found:
-            delimiter_index = recvData.find(b'\r\n\r\n')  # Find the position of the delimiter
-            if delimiter_index != -1:
-                total_received = len(recvData) - (delimiter_index + len(b'\r\n\r\n'))  # Update total received bytes counter
-                delimiter_found = True
-        else:
-            total_received += len(chunk)
-        
-        if b'Content-Length:' in recvData:
-            # Find the position of the Content-Length header
-            content_length_index = recvData.find(b'Content-Length:')
-            # Find the end of the line after the Content-Length header
-            end_of_line_index = recvData.find(b'\r\n', content_length_index)
-            if end_of_line_index >= 0:
-                # Extract the substring containing the value of Content-Length
-                content_length_str = recvData[content_length_index:end_of_line_index]
-                # Split the string to get the value after the colon
-                content_length_value = content_length_str.split(b':')[1].strip()
-                # Convert the value to an integer
-                content_length = int(content_length_value)
 
-                # Check if total received bytes is greater than or equal to content length
+    while True:
+        # Use select to wait for the socket to be ready to read
+        readable, _, _ = select.select([clientSock], [], [], 5.0)  # 5.0 seconds timeout
+        if not readable:  # If the socket is not ready to read after the timeout
+            print("Timeout or no data available to read.")
+            break
+
+        for sock in readable:
+            chunk = sock.recv(BUF_SIZE)
+            if not chunk:
+                break
+            recvData += chunk
+            if not delimiter_found:
+                delimiter_index = recvData.find(b'\r\n\r\n')  # Find the position of the delimiter
+                if delimiter_index != -1:
+                    total_received = len(recvData) - (delimiter_index + 4)  # Update total received bytes counter
+                    delimiter_found = True
+            else:
+                total_received += len(chunk)
+
+            if b'Content-Length:' in recvData:
+                content_length = int(recvData.split(b'Content-Length: ')[1].split(b'\r\n')[0])
                 if total_received >= content_length:
                     break  # Exit the loop once enough data has been received
-        if args.method == "HEAD" and b'\r\n\r\n' in recvData:
-            break
+            if args.method == "HEAD" and b'\r\n\r\n' in recvData:
+                break
     
     header_data, content_data = split_header_content(recvData)
     
@@ -100,6 +99,7 @@ def sendReq(request,clientSock,dep):
     
     with open(dep, 'wb') as f:
         f.write(content_data)
+
 
 def split_header_content(data):
     header_end = data.find(b'\r\n\r\n') + 4  # Find the end of header (including \r\n\r\n)
